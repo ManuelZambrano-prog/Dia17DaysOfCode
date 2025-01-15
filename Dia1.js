7effb155707afa2e89e86a50a17cbcba6fb79600
ManuelZambrano-prog <mszambrano855@gmail.com>
2025-01-15 17:02:08 -0500

    Se agrega nuevo proyecto

---
+++ Dia1.js
@@ -0,0 +1,50 @@
+let numeroUn = 1
+
+let stringUn = '1'
+
+let numeroTreinta = 30
+
+let stringTreinta = '30'
+
+let numeroDiez = 10
+
+let stringDiez = '10'
+
+
+
+
+if (numeroUn==stringUn) {
+//para comprobar este resultado solo se debe cambiar el valor de la variable arriba y listo
+  console.log('Las variables numeroUn y stringUn tienen el mismo valor, pero tipos diferentes')
+
+} 
+else {
+
+  console.log('Las variables numeroUn y stringUn no tienen el mismo valor')
+
+}
+
+ 
+
+if (numeroTreinta==stringTreinta) {
+//para comprobar este resultado se le coloca un tercer "=" para que el if lo tome como que los valores deber ser identicos
+  console.log('Las variables numeroTreinta y stringTreinta tienen el mismo valor y el mismo tipo')
+
+} else {
+
+  console.log('Las variables numeroTreinta y stringTreinta no tienen el mismo tipo')
+
+}
+
+ 
+
+if (numeroDiez==stringDiez ) {
+  //para comprobar este resultado simplemente se debe cambiar el valor que se le asignó a la variable
+
+  console.log('Las variables numeroDiez y stringDiez tienen el mismo valor, pero tipos diferentes')
+
+} else {
+
+  console.log('Las variables numeroDiez y stringDiez no tienen el mismo valor')
+
+}
